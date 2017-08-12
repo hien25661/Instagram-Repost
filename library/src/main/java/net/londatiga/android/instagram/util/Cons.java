a9261d0c3e194bfcee506c3ee2117c0b3231546a
HienNV <hien.nv@vn.gnt-global.com>
2017-08-12 18:43:57 +0700

    Add permission

---
+++ library/src/main/java/net/londatiga/android/instagram/util/Cons.java
@@ -0,0 +1,15 @@
+package net.londatiga.android.instagram.util;
+
+/**
+ * Constants.
+ * 
+ * @author Lorensius W. L. T <lorenz@londatiga.net>
+ *
+ */
+public class Cons {
+	public static final String ACCESS_TOKEN_URL = "https://api.instagram.com/oauth/access_token";
+	public static final String API_BASE_URL = "https://i.instagram.com/api/v1/";
+	public static final String AUTH_URL = "https://api.instagram.com/oauth/authorize/?";
+	public static final boolean ENABLE_DEBUG = true;
+	public static final String TAG = "AndroidInstagram";
+}
