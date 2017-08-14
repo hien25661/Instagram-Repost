940da693aac94733ddc43ae126c0446d344507ce
HienNV <hien.nv@vn.gnt-global.com>
2017-08-14 23:20:01 +0700

    Add Lib

---
+++ paper-onboarding/src/test/java/com/ramotion/paperonboarding/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.ramotion.paperonboarding;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleUnitTest {
+    @Test
+    public void additionIsCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
