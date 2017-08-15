3a70e7fc63402988dcdfcc849cc1cc27e5983405
HienNV <hien.nv@vn.gnt-global.com>
2017-08-15 09:43:50 +0700

    Add Full Banner

--- app/src/main/java/com/sns/repost/activities/SplashActivity.java
+++ app/src/main/java/com/sns/repost/activities/SplashActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.sns.repost.BaseActivity;
 import com.sns.repost.R;
 
@@ -13,26 +16,48 @@ import com.sns.repost.R;
  */
 
 public class SplashActivity extends BaseActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 3500;
+    InterstitialAd interstitial;
+    AdRequest adRequest;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        adRequest = new AdRequest.Builder().build();
+
+        interstitial = new InterstitialAd(this);
+        // Insert the Ad Unit ID
+        interstitial.setAdUnitId(getResources().getString(R.string.ads_id_interstis));
+        interstitial.loadAd(adRequest);
+        interstitial.setAdListener(new AdListener() {
+            // Listen for when user closes ad
+            public void onAdClosed() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        });
+
         new Handler().postDelayed(new Runnable() {
 
 			/*
-			 * Showing splash screen with a timer. This will be useful when you
+             * Showing splash screen with a timer. This will be useful when you
 			 * want to show case your app logo / company
 			 */
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                // close this activity
-                finish();
+                // Load ads into Interstitial Ads
+                if (interstitial.isLoaded()){
+                    interstitial.show();
+                }else {
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    // close this activity
+                    finish();
+                }
             }
         }, SPLASH_TIME_OUT);
     }
